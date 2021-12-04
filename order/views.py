98954e0a71f70c12a88d89bf78c2e9e16d11db8e
Nicolas Alvarez <X00181493@mytudublin.ie>
2021-12-04 10:02:41 +0000

    order app completed

--- order/views.py
+++ order/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Order, OrderItem
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
 
 # Create your views here.
+class orderHistory(LoginRequiredMixin, View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            email = str(request.user.email)
+            order_details = Order.objects.filter(emailAddress=email)
+        return render(request, 'order/order_list.html', {'order_details':order_details})
+
+class orderDetail(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        if request.user.is_authenticated:
+            email = str(request.user.email)
+            order = Order.objects.get(id=order_id, emailAddress=email)
+            order_items = OrderItem.objects.filter(order=order)
+
+        return render(request, 'order/order_detail.html', {'order':order, 'order_items':order_items})
