644fa3ccd5d44c6d188b76815fb236fd37709098
Abdullah Zaidi <X00177647@mytudublin.ie>
2021-12-21 01:20:16 +0000

    fixed some more html for the products

--- shop/models.py
+++ shop/models.py
@@ -32,6 +32,7 @@ class Product(models.Model):
     
     name = models.CharField(max_length=250, unique=True)
     description = models.TextField(blank=True)
+    description_two = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='product', blank=True)
